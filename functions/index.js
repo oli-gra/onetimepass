f966d8dfaabc10ee89ee0ac9b25b93b3c30d83c5
Home <home@Homes-MBP.broadband>
2020-08-11 11:51:42 +0100

    user creation

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,11 @@
+const admin = require('firebase-admin')
 const functions = require('firebase-functions');
+const createUser = require('./create_user')
+const serviceAccount = require('./service_account.json')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello World");
-});
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://onetimepass-8a4de.firebaseio.com"
+  })
+
+exports.createUser = functions.https.onRequest(createUser)
